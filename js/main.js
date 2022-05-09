d8f4d33ac4f09fa07c460d4867f8474d18db2a31
pashayeva008 <dkhanimp@gmail.com>
2022-05-09 14:36:22 +0400

    final

--- js/main.js
+++ js/main.js
@@ -1,14 +1,42 @@
 'use strict'
-let  = document.querySelector(".stylecol .fredHyman")
-let play = document.querySelectorAll('.stylecol .fredHyman  .fapIcon i')
-let close = document.querySelector(".stylecol .fredHyman i")
+
+// for (i = 0; i < side.length; i++) {
+//   side[i].addEventListener("click", function() {
+  
+//     this.classList.toggle("active");
+
+//     var panel = this.nextElementSibling;
+//     if (menu.style.display === "block") {
+//       menu.style.display = "none";
+//     } else {
+//       menu.style.display = "block";
+//     }
+//   });
+// }
 
 
-for (let i = 0; i < play.length; i++) {
-    play[i].addEventListener("click", function () {
-        videoBox.classList.add("activeVideo")
-    })
+
+
+
+
+
+
+
+
+
+
+
+
+let btnc =document.querySelectorAll(".thtorial  .leftorial ul li");
+let bd =document.querySelectorAll(" .thtorial .alltutorial .leftorial .tbover");
+for(let i =0 ;i < btnc.length; i++){
+    btnc[i].onclick = function () {
+        for (let j = 0; j < bd.length; j++) {
+          bd[j].classList.remove("active");
 }
-close.addEventListener("click", function () {
-    videoBox.classList.remove("activeVideo")
-})
+var btni = this.getAttribute("tab");
+bd[btni].classList.add("active")
+}
+
+}
+
